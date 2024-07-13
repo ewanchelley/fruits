43f0cc2dde1d34334ad9da7421507a0830c08872
Ewan Chelley <ewan.chelley@gmail.com>
2024-07-13 13:40:51 +0200

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Ewan Chelley on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
